ac67422316a8ac4461fea12a851ceb8ed3a80a94
majo <130527406+majitomoomin@users.noreply.github.com>
2024-02-23 17:23:44 -0600

    Add files via upload

---
+++ ejemplo_4.cs
@@ -0,0 +1,19 @@
+﻿namespace LeerTeclado
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("suma de dos numeros");
+            Console.Write("ingresa número 1: ");
+            string dato = Console.ReadLine();
+            int n1 = int.Parse(dato);
+
+            Console.Write("Ingresa número 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("El resultado es: {0}", suma);
+        }
+    }
+}
