f54a43e4f0a9100943e1b6d9b4f7c97380bc6091
majo <130527406+majitomoomin@users.noreply.github.com>
2024-02-23 16:56:54 -0600

    Add files via upload

---
+++ primer_ejemplo.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Fundamentos
+{
+    class Miprimerprograma
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo isi, haz click");
+        }
+    }
+}
