c26ab68ad17cc6f83cc9519efe770265c1c4d7c7
majo <130527406+majitomoomin@users.noreply.github.com>
2024-02-23 18:12:20 -0600

    Add files via upload

---
+++ ejemplo5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sentencias
+{
+    class SentenciaIf
+    {
+        public static void Main(string[]args)
+        {
+            int numeroentero = int.Parse(Console.ReadLine());
+
+            if (numeroentero == 7)
+                Console.WriteLine("Numero es 7 ");
+            else if (numeroentero < 0)
+                Console.WriteLine("EL numro es negativo ");
+            else
+                Console.WriteLine("El numero no es ninguno de los casos programados");
+            {
+                
+            }
+        }
+    }
+}
